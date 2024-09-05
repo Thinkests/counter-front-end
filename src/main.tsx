b5f1e5eb6e08a1373ada75df5d80e02314112a16
Thinkests <thinkests@gmail.com>
2024-09-04 19:46:19 -0600

    Fix page url

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const manifestUrl = "https://thinkests.github.jo/counter-front-end/tonconnect-manifest.json";
+const manifestUrl = "https://thinkests.github.io/counter-front-end/tonconnect-manifest.json";
 
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
